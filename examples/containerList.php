40edba3094207b80c5ceafdeea0f369490bb827b
Tunahan Caliskan <mail@tunahancaliskan.com>
2021-08-30 18:26:48 +0300

    ReactPHP added/

--- examples/containerList.php
+++ examples/containerList.php
@@ -4,9 +4,36 @@ use Tnhnclskn\Docker\Docker;
 
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
-$docker = Docker::create();
-$containers = $docker->containerList();
+require __DIR__ . '/vendor/autoload.php';
 
-foreach ($containers as $container) {
-    var_dump($container->getNames());
-}
+$proxy = new Clue\React\SshProxy\SshProcessConnector('root@docker.clskn.net');
+
+$connector = new React\Socket\Connector(array(
+    'tcp' => $proxy,
+    'dns' => false
+));
+
+$browser = new React\Http\Browser($connector);
+
+$browser->get('https://google.com/')->then(function (Psr\Http\Message\ResponseInterface $response) {
+    var_dump($response->getHeaders(), (string) $response->getBody());
+}, function (Exception $e) {
+    echo 'Error: ' . $e->getMessage() . PHP_EOL;
+});
+
+// $connector = new React\Socket\FixedUriConnector(
+//     'unix:///var/run/docker.sock',
+//     new React\Socket\UnixConnector()
+// );
+
+// $browser = new React\Http\Browser($connector);
+
+// $client->get('http://localhost/info')->then(function (Psr\Http\Message\ResponseInterface $response) {
+//     var_dump($response->getHeaders(), (string)$response->getBody());
+// });
+// $docker = Docker::create();
+// $containers = $docker->containerList();
+
+// foreach ($containers as $container) {
+//     var_dump($container->getNames());
+// }
